5bf89aa7fdfd4da9f9f690ff605b03826984c317
Hunter Koppen <hunter.koppen@kobeon.nl>
2024-10-25 19:01:14 +0200

    bugfixes

--- src/components/PickerContainer.jsx
+++ src/components/PickerContainer.jsx
@@ -3,20 +3,25 @@ import Picker from "react-mobile-picker";
 
 export class PickerContainer extends Component {
     state = {
-        pickerValue: ""
+        pickerValue: {
+            group: ""
+        }
     };
 
     items = [];
 
     componentDidUpdate(prevProps) {
-        const { itemsDatasource } = this.props;
+        const { itemsDatasource, attributeAssociation } = this.props;
         if (prevProps.itemsDatasource?.status === "loading" && itemsDatasource?.status === "available") {
             this.generateItems();
+        }
+        if (attributeAssociation.value && attributeAssociation.value.id !== this.state.pickerValue.group) {
+            this.setState({ pickerValue: { group: attributeAssociation.value.id } });
         }
     }
 
     generateItems = () => {
-        const { itemsDatasource, itemContent } = this.props;
+        const { itemsDatasource, itemContent, attributeAssociation } = this.props;
         this.items = [];
 
         itemsDatasource.items.forEach(mxObject => {
@@ -29,16 +34,24 @@ export class PickerContainer extends Component {
             this.items.push(item);
         });
 
-        if (this.items.length > 0) {
-            this.setState({ pickerValue: this.items[0].id });
+        // Get the object currently associated and set that as the initial value, if empty simply set it to the first item in the list
+        if (attributeAssociation.value) {
+            const initialItem = this.items.find(item => item.id === attributeAssociation.value.id);
+            if (initialItem) {
+                this.setState({ pickerValue: { group: initialItem.id } });
+            }
+        } else if (this.items.length > 0) {
+            this.setState({ pickerValue: { group: this.items[0].id } });
         }
     };
 
-    setPickerValue = (name, value) => {
-        // Check if the new value exists in items before updating the state
-        const isValidValue = this.items.some(item => item.id === value);
-        if (isValidValue) {
-            this.setState({ pickerValue: value });
+    setPickerValue = (value, key) => {
+        const { attributeAssociation } = this.props;
+
+        const selectedItem = this.items.find(item => item.id === value.group);
+        if (selectedItem) {
+            this.setState({ pickerValue: { [key]: value.group } });
+            //attributeAssociation.setValue(selectedItem.id);
         }
     };
 
@@ -47,7 +60,7 @@ export class PickerContainer extends Component {
 
         return (
             <Picker value={pickerValue} onChange={this.setPickerValue}>
-                <Picker.Column name="Group">
+                <Picker.Column name="group">
                     {this.items.map(item => (
                         <Picker.Item key={item.id} value={item.id}>
                             {({ selected }) => <div className={selected ? "selected" : ""}>{item.content}</div>}
